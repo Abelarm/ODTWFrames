28b367b55448232b5e3f5131734dd05951981910
Luigi G <luigi3000@gmail.com>
2020-05-08 15:24:54 +0200

    improved the path class, added a new runner for ResNet in 1D

---
+++ src/runners/TS_1DResNet/evaluate_DTW_ResNet.py
@@ -0,0 +1,44 @@
+from comet_ml import Experiment
+
+from utils.functions import Paths
+from utils.specification import specs
+from models.generator_proxy import create_generator
+from models.network import Network
+from models.ResNet1D.model import get_model, optimizer
+
+dataset = 'cbf'
+dataset_type = 'TS'
+window_size = 5
+
+y_dim = specs[dataset]['y_dim']
+x_dim = specs[dataset]['x_dim']
+
+parameters = dict()
+parameters['batch_size'] = 32
+parameters['preprocessing'] = True
+parameters['reload_images'] = False
+
+
+paths = Paths(dataset, dataset_type, False, window_size, False, '')
+
+data_path = paths.get_data_path()
+weight_dir = paths.get_weight_dir()
+
+# Add the following code anywhere in your machine learning file
+project_name = f'TS_ResNet_{dataset}'
+
+experiment = Experiment(api_key="tIjRDRXwqoq2RgkME4epGXp1C",
+                        project_name=project_name, workspace="luigig",
+                        disabled=True)
+
+NN = Network(data_path,
+             x_dim=(window_size, 1), y_dim=y_dim,
+             model_name=f'DTW_CNN_{window_size}.hdf5', experiment=experiment)
+
+NN.init_model(get_model, parameters, optimizer, create_generator)
+# NN.train(epochs=1, save_path=weight_dir, from_checkpoint=False)
+NN.evaluate(weights_dir=weight_dir)
+NN.summary_experiments(weights_dir=weight_dir, dataset_name=dataset)
+NN.error_analysis(weights_dir=weight_dir, dataset_name=dataset)
+
+experiment.end()
--- src/utils/functions.py
+++ src/utils/functions.py
@@ -40,6 +40,9 @@ class Paths(metaclass=Singleton):
     pattern_name = None
     core_path = None
 
+    rho_name = ''
+    model_name = ''
+
     def __init__(self, dataset, dataset_type, rho, window_size, base_pattern, pattern_name, core_path='../../..'):
 
         self.dataset = dataset
@@ -50,46 +53,8 @@ class Paths(metaclass=Singleton):
         self.pattern_name = pattern_name
         self.core_path = core_path
 
-    def get_beginning_path(self):
-
-        return f'{self.core_path}/data/{self.dataset}'
-
-    def get_dtw_path(self):
-
-        beginning_path = self.get_beginning_path()
-
-        dtw_path = f'{beginning_path}/rho {self.rho}/'
-        if self.base_pattern:
-            dtw_path = f'{beginning_path}/rho {self.rho}_base/'
-            if len(self.pattern_name) > 0:
-                dtw_path = f'{beginning_path}/rho {self.rho}_base_{self.pattern_name}'
-
-        return dtw_path
-
-    def get_data_path(self):
-
-        beginning_path = self.get_beginning_path()
-
-        data_path = f'{beginning_path}/rho {self.rho}/{self.dataset_type}_{self.window_size}'
-        if self.base_pattern:
-            data_path = f'{beginning_path}/rho {self.rho}_base/{self.dataset_type}_{self.window_size}'
-            if len(self.pattern_name) > 0:
-                data_path = f'{beginning_path}/rho {self.rho}_base_{self.pattern_name}/' \
-                            f'{self.dataset_type}_{self.window_size}'
-
-        return data_path
-
-    def get_weight_dir(self):
-
-        weight_dir = f'{self.core_path}/Network_weights/{self.dataset}/rho {self.rho}'
-        if self.base_pattern:
-
-            weight_dir = f'{self.core_path}/Network_weights/{self.dataset}/rho {self.rho}_base/'
-            if len(self.pattern_name) > 0:
-
-                weight_dir = f'{self.core_path}/Network_weights/{self.dataset}/rho {self.rho}_base_{self.pattern_name}/'
-
-        return weight_dir
+        self.get_rho_name()
+        self.get_model_name()
 
     def get_rho_name(self):
 
@@ -102,32 +67,53 @@ class Paths(metaclass=Singleton):
         else:
             rho_name = ''
 
+        self.rho_name = rho_name
         return rho_name
 
     def get_model_name(self):
+        self.model_name = f'{self.dataset_type}_{self.window_size}'
         return f'{self.dataset_type}_{self.window_size}'
+
+    def get_beginning_path(self):
+
+        return f'{self.core_path}/data/{self.dataset}'
+
+    def get_dtw_path(self):
+
+        beginning_path = self.get_beginning_path()
+
+        dtw_path = f'{beginning_path}/{self.rho_name}'
+        return dtw_path
+
+    def get_data_path(self):
+
+        beginning_path = self.get_beginning_path()
+
+        data_path = f'{beginning_path}/{self.rho_name}/{self.model_name}'
+        return data_path
+
+    def get_weight_dir(self):
+
+        if not self.rho:
+            weight_dir = f'{self.core_path}/Network_weights/{self.dataset}/{self.dataset_type}'
+        else:
+            weight_dir = f'{self.core_path}/Network_weights/{self.dataset}/{self.rho_name}'
+
+        return weight_dir
 
     def get_summaries_path(self):
 
-        rho_name = self.get_rho_name()
-
-        path_dir = f'{self.core_path}/experiment_summaries/{self.dataset}/{rho_name}/' \
-                   f'{self.dataset_type}_{self.window_size}'
+        path_dir = f'{self.core_path}/experiment_summaries/{self.dataset}/{self.rho_name}/{self.model_name}'
 
         return path_dir
 
     def get_error_path(self):
-        rho_name = self.get_rho_name()
 
-        path_dir = f'{self.core_path}/error_analysis/{self.dataset}/{rho_name}/' \
-                   f'{self.dataset_type}_{self.window_size}'
+        path_dir = f'{self.core_path}/error_analysis/{self.dataset}/{self.rho_name}/{self.model_name}'
 
         return path_dir
 
     def get_explain_path(self):
-        rho_name = self.get_rho_name()
 
-        path_dir = f'{self.core_path}/Network_explain/{self.dataset}/{rho_name}/' \
-                   f'{self.dataset_type}_{self.window_size}'
-
+        path_dir = f'{self.core_path}/Network_explain/{self.dataset}/{self.rho_name}/{self.model_name}'
         return path_dir
